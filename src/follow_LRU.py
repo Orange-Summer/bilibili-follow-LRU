8772eb82ca94e60e392d29dffe3f389bfe418215
Orange Summer <orangesummerfxt@gmail.com>
2025-03-07 00:53:22 +0800

    :bug: Fix incorrect log output

--- src/follow_LRU.py
+++ src/follow_LRU.py
@@ -13,9 +13,9 @@ def iterate_users():
     three_month_ago_time = curr_time - 30 * 24 * 60 * 60 * 3
     cursor.execute('SELECT name, mid FROM users WHERE latest_time > 0 AND latest_time < ?', (one_month_ago_time,))
     one_month_ago = cursor.fetchall()
-    logger.info(f"一个月观看的up有{len(one_month_ago)}名")
+    logger.info(f"一个月未观看的up有{len(one_month_ago)}名")
     cursor.execute('SELECT name, mid FROM users WHERE latest_time > 0 AND latest_time < ?', (three_month_ago_time,))
     three_month_ago = cursor.fetchall()
-    logger.info(f"三个月观看的up有{len(one_month_ago)}名")
+    logger.info(f"三个月未观看的up有{len(one_month_ago)}名")
     conn.close()
     return one_month_ago, three_month_ago
